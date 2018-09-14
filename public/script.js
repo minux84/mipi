1ab403730e7e233fe2ca5b953fe5fc2a8090e155
minux84 <sav.carmine@gmail.com>
2018-09-14 22:48:44 +0200

    first commit

---
+++ public/script.js
@@ -0,0 +1,89 @@
+var socket = null;
+
+$(function () {
+    socket = io('http://192.168.1.111:3000');
+	
+	socket.emit('init');
+	
+	socket.on('devices', function(data){				
+		buildList(".devices-list", data);
+		initList();
+	});
+	
+	socket.on('restoreState', function(data){
+
+		buildList(".devices-out",data.device_out);
+		buildList(".devices-in",data.device_in);
+		initList();
+	});
+
+});
+
+function buildList(listContainer, data){
+
+    var html = "";
+    
+	data.forEach(function(data){
+		html += "<li class='list-group-item' data-val='"+data.val+"'>"+data.label+"</li>";
+	});
+	
+	$(listContainer).html(html);	            
+}
+
+function initList(){
+    
+	var options = { 
+		group: "devices",
+		onEnd: function (evt) {
+			sendData(); 
+		}
+	};
+
+	for (const list of document.querySelectorAll("ol.devices")) {
+	    Sortable.create(list, options);
+	}
+	
+}
+
+
+function sendData(){
+    var data = [];
+    var currentState = {device_out: [], device_in: []};
+    
+    $(".devices-in li.list-group-item").each(function(){ //ottimizzare ed evitare la connessione con lo stesso strumento
+        var device_in = this;
+        if(device_in){
+            
+            
+        	$(".devices-out li.list-group-item").each(function(){
+	        	var device_out = this;
+
+	        	if(device_out){
+					
+		        	data.push("aconnect "+$(device_out).attr('data-val')+"0"+" "+$(device_in).attr('data-val')+"0");					        	
+	        	}
+	        	
+        	});   			            
+        }
+
+    });
+    
+    if(data.length > 0){
+    	socket.emit("midiSetup", data);
+    }else{
+	    socket.emit("resetMidi");
+    }
+    
+    $(".devices-in li.list-group-item").each(function(){
+        var device_in = this;
+        currentState.device_in.push({ val: $(device_in).attr('data-val'), label: $(device_in).text() });
+    });
+    
+    $(".devices-out li.list-group-item").each(function(){
+	    var device_out = this;
+		currentState.device_out.push({ val: $(device_out).attr('data-val'), label: $(device_out).text() });
+    });
+    
+    socket.emit("saveState", currentState);
+
+}
---
+++ server.js
@@ -0,0 +1,130 @@
+#!/usr/bin/env node
+
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+var readline = require('readline');
+var fs = require('fs');
+var exec = require('child_process').exec;
+
+
+
+var initDevices = function(socket, firstInit = false, emit = false){
+	exec("aconnect -l | grep client > "+__dirname+"midi_devices.txt", function (error, stdout, stderr) { 
+		
+		var devices = [];
+		
+		const rl = readline.createInterface({
+		  input: fs.createReadStream(__dirname+'/midi_devices.txt'),
+		  crlfDelay: Infinity
+		});  
+		
+		var i = 0;
+		rl.on('line', (line) => {
+			if(i > 1){
+				devices.push({
+					"val":line.split(" ")[1],
+					"label":line.split("'")[1]	
+				});				
+			}
+			
+			i++;
+		}); 
+		
+		
+		rl.on('close', () => {
+			if(emit)
+				socket.emit('devices', devices);
+				
+			if(firstInit){
+				const rl = readline.createInterface({
+				  input: fs.createReadStream(__dirname+'/init.txt'),
+				  crlfDelay: Infinity
+				});  
+				
+				rl.on('line', (line) => {
+					exec(line, null);
+				}); 				
+			}
+			
+		});	
+		
+	});
+	
+	//restoreData(socket);			
+
+};
+
+var restoreData = function(socket){
+	
+	if(fs.existsSync(__dirname +'savedState.json')){
+		var savedState = JSON.parse(fs.readFileSync('savedState.json', 'utf8'));
+		
+		if(savedState){
+			socket.emit('restoreState', savedState);
+		}				
+	}
+	
+};
+
+
+initDevices(null, true);
+
+app.use('/static', express.static(__dirname+'/public'));
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname+'/index.html');
+});
+
+io.on('connection', function(socket){
+	
+	
+	socket.on('init', function(data){
+
+		initDevices(socket, false, true);		
+		
+		if(fs.existsSync(__dirname+'/savedState.json')){
+			var savedState = JSON.parse(fs.readFileSync(__dirname+'/savedState.json', 'utf8'));
+			
+			if(savedState){
+				socket.emit('restoreState', savedState);
+			}				
+		}
+
+	});
+		
+ 
+    socket.on('midiSetup', function (data) {
+	    exec("aconnect -x");
+	    
+	    var connections = "";
+        
+        data.forEach(function(connection){
+	        exec(connection, null);
+			connections += connection+"\n";
+			
+        });
+
+        fs.writeFileSync(__dirname+'/init.txt', connections);
+    });
+
+	
+	socket.on('saveState', function (data) {
+        fs.writeFileSync(__dirname+'/savedState.json', JSON.stringify(data));
+        initDevices(socket, false, true);
+	});
+	
+	socket.on('resetMidi', function(){
+		exec("aconnect -x");
+	});
+
+});
+  
+http.listen(3000, function () {       
+  console.log('Server started. Listening on *:3000');
+});
+
+
+
